6505b9b20f82449c39b529c0ad9c6fc38c61de92
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2021-05-17 18:25:41 -0500

    created constant for number of allowed guesses

--- src/com/hervey/app/AppMastermind.java
+++ src/com/hervey/app/AppMastermind.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class AppMastermind {
+	public static final int numberOfAllowedGuesses = 2;  //should be 8 for production code
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 		Mastermind codemaker = new Mastermind();
-		for (int i = 1; i <= 8; i++) {
+		displayWelcome(numberOfAllowedGuesses);
+		for (int i = 1; i <= numberOfAllowedGuesses; i++) {
 			System.out.print("Guess: " + i + ": ");
 			String guess = stdin.readLine();
 			codemaker.reportMatches(guess);
@@ -19,5 +21,9 @@ public class AppMastermind {
 			}
 		}
 		System.out.println("The correct code is " + codemaker.getCode());
+	}
+
+	private static void displayWelcome(int maxGuesses) {
+		System.out.println("There is a secret code consisting of 4 letters\nYou have " + maxGuesses + " guesses to try for a match");
 	}
 }
